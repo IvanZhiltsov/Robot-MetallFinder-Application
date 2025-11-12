80a7abf974be323e7e0d927408e35fb67c23311c
IvanZhiltsov <ivanzh08@gmail.com>
2025-11-13 01:20:20 +0300

    Update MapPy - create def setMap

--- MapPy.py
+++ MapPy.py
@@ -1,2 +1,10 @@
 class Map:
-    pass
+    def __init__(self):
+        self.name = None
+        self.type = "empty"
+        self.data = None
+
+    def setMap(self, name, map_txt):
+        self.name = name
+        self.type = "data"
+        self.data = map_txt
