02eab501db0a05517d34a0ae9c8d64d3ea4af71e
IvanZhiltsov <ivanzh08@gmail.com>
2025-11-13 21:24:50 +0300

    Create LoadFileDialog

--- MapPy.py
+++ MapPy.py
@@ -1,10 +1,13 @@
 class Map:
-    def __init__(self):
-        self.name = None
-        self.type = "empty"
-        self.data = None
+    def __init__(self, map_text=None, name=None):
+        if map_text is None:
+            self.name = name
+            self.type = "empty"
+            self.data = None
+        else:
+            self.setMap(map_text, name)
 
-    def setMap(self, name, map_txt):
+    def setMap(self, map_txt, name=None):
         self.name = name
-        self.type = "data"
+        self.type = map_txt.rstrip()
         self.data = map_txt
--- main_fragmented.py
+++ main_fragmented.py
@@ -4,7 +4,7 @@ from BluetoothPy import Bluetooth
 from MapPy import Map
 
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QDialog
 from PyQt6 import QtCore, QtWidgets
 
 from PyQt6.QtWidgets import QPushButton, QButtonGroup, QFileDialog
@@ -17,11 +17,11 @@ if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
 bluetooth = Bluetooth()
-map = Map()
+curr_map = Map()
 
 
 class MainWindow(QMainWindow):
-    global bluetooth, map
+    global bluetooth, curr_map
 
     def __init__(self):
         super().__init__()
@@ -33,18 +33,29 @@ class MainWindow(QMainWindow):
         self.tab_map = TabMap(self)
         self.menu = Menu(self)
 
+        self.load_file_dialog = None
+
+        self.status_text = None
+
     def update(self):
         self.tab_robot.update()
         self.tab_map.update()
         self.menu.update()
 
-        if map.name is not None:
-            self.setWindowTitle(f"Robot-MetallFinder connector - [{map.name}]")
+        if self.status_text is None:
+            self.statusBar().clearMessage()
+        else:
+            self.statusBar().showMessage(self.status_text)
+        self.status_text = None
+
+        if curr_map.name is not None:
+            self.setWindowTitle(f"Robot-MetallFinder connector - [{curr_map.name}]")
         else:
             self.setWindowTitle(f"Robot-MetallFinder connector")
 
     def load_file(self):
-        pass
+        self.load_file_dialog = LoadFileDialog(self)
+        self.load_file_dialog.show()
 
 
 class TabMap:
@@ -67,9 +78,7 @@ class TabMap:
         self.update()
 
     def update(self):
-        if map.type == "empty":
-            self.p.edit_map_widget.show()
-        elif map.type == "edit":
+        if curr_map.type in ("empty", "edit"):
             self.p.edit_map_widget.show()
         else:
             self.p.edit_map_widget.hide()
@@ -231,28 +240,113 @@ class Menu:
         self.p.action_open.triggered.connect(self.open_file)
         self.p.action_save.triggered.connect(self.save_file)
         self.p.action_save_as.triggered.connect(self.save_as_file)
-        self.p.action_load.triggered.connect(self.p.load_file)
+        self.p.action_load.triggered.connect(self.load_file)
 
         self.update()
 
     def update(self):
-        if bluetooth.is_connected():
+        if bluetooth.is_connected() and curr_map.type == "edit":
             self.p.action_load.setEnabled(True)
         else:
             self.p.action_load.setEnabled(False)
 
     def open_file(self):
-        filename = QFileDialog.getOpenFileName(self.p, "Открыть файл", '')[0]
-        with open(filename, mode='r', encoding='utf-8') as file:
-            map_text = file.read().rstrip()
-            map.setMap(filename, map_text)
-        self.p.update()
+        filename, ok = QFileDialog.getOpenFileName(self.p, "Открыть файл", '')
+        if ok:
+            with open(filename, mode='r', encoding='utf-8') as file:
+                map_text = file.read()
+                curr_map.setMap(map_text, filename)
+            self.p.update()
 
     def save_file(self):
         pass
 
     def save_as_file(self):
         pass
+
+    def load_file(self):
+        self.p.load_file()
+        self.p.load_file_dialog.open_current_file()
+
+
+class LoadFileDialog(QDialog):
+    global bluetooth, curr_map
+
+    def __init__(self, parent):
+        super().__init__()
+        uic.loadUi('LoadFileDialog.ui', self)
+
+        self.parent = parent
+        self.geo_map = None
+        self.status_text = None
+
+        self.cancel_btn.clicked.connect(self.close)
+        self.open_btn.clicked.connect(self.open_file)
+        self.open_current_btn.clicked.connect(self.open_current_file)
+        self.push_file_btn.clicked.connect(self.push_file)
+
+        self.update()
+
+    def update(self):
+        if self.status_text is None:
+            self.status_label.clear()
+            self.status_label.hide()
+        else:
+            self.status_label.setText(self.status_text)
+            self.status_label.show()
+        self.status_text = None
+
+        if self.geo_map is None:
+            self.push_file_btn.hide()
+            self.info_widget.hide()
+            self.name_label.hide()
+        else:
+            self.push_file_btn.show()
+            self.info_widget.show()
+            self.name_label.show()
+
+            self.update_info()
+
+    def update_info(self):
+        if self.geo_map.name is None:
+            self.name_label.setText("Без имени")
+        else:
+            self.name_label.setText(self.geo_map.name)
+
+    def open_file(self):
+        filename, ok = QFileDialog.getOpenFileName(self, "Выбрать карту", '')
+        if ok:
+            with open(filename, mode='r', encoding='utf-8') as file:
+                map_text = file.read()
+                self.geo_map = Map(map_text, filename)
+
+            if self.geo_map.type != "edit":
+                self.geo_map = None
+                self.status_text = "Неверный тип карты!!"
+
+            self.update()
+
+    def open_current_file(self):
+        if curr_map.type != "edit":
+            self.geo_map = None
+            self.status_text = "Неверный тип карты!!"
+        else:
+            self.geo_map = curr_map
+
+        self.update()
+
+    def push_file(self):
+        if bluetooth.is_connected():
+            if bluetooth.push_file():
+                self.parent.update()
+                self.close()
+            else:
+                self.status_text = "Не удалось отправить инструкцию"
+                self.update()
+        else:
+            self.parent.status_text = "Не удалось отправить инструкцию - нет подключения к Bluetooth!!"
+            self.parent.update()
+            self.close()
 
 
 def except_hook(cls, exception, traceback):
